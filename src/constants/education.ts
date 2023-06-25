e9a6316e2f63ea957601377be6b88d95404489fc
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-25 20:29:54 +0330

    something changed...

--- src/constants/education.ts
+++ src/constants/education.ts
@@ -1,9 +1,9 @@
-export type educationItem= {
-year: string
-cert: string
-major: string
-place: string
-}
+export type educationItem = {
+  year: string;
+  cert: string;
+  major: string;
+  place: string;
+};
 
 const education: educationItem[] = [
   {
@@ -14,7 +14,7 @@ const education: educationItem[] = [
   },
   {
     year: "1400 - 1402",
-    cert: "فوق دیپلم - در حال تحصیل",
+    cert: "فوق دیپلم",
     major: "رشته کامپیوتر - نرم افزار",
     place: "دانشکده فنی و حرفه ای شهید چمران رشت",
   },
