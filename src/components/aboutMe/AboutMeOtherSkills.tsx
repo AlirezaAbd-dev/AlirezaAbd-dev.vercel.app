d0bb911fdbdf079700cb11997ed47f24904bdf65
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-27 14:34:44 +0330

    colors and icons changed...

--- src/components/aboutMe/AboutMeOtherSkills.tsx
+++ src/components/aboutMe/AboutMeOtherSkills.tsx
@@ -1,20 +1,20 @@
 "use client";
-import { SelfImprovementRounded } from "@mui/icons-material";
 import { Box } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import otherSkillsData from "../data/otherSkillsData";
 import HeaderDivider from "../ui/HeaderDivider";
 import AboutMeSkillItem from "./AboutMeSkillItem";
+import SkateboardingIcon from "@mui/icons-material/Skateboarding";
 
 const AboutMeOtherSkills = () => {
   return (
     <Grid container>
       <Grid sx={{ width: 1, mt: 1 }}>
         <HeaderDivider
-          color="primary.dark"
+          color="greenAccent.main"
           animation={false}
           // @ts-ignore
-          icon={<SelfImprovementRounded color="text.primary" />}
+          icon={<SkateboardingIcon color="text.primary" />}
         >
           سایر مهارت ها
         </HeaderDivider>
