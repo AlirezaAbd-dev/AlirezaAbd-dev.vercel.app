151b64f16a4a64756821a8dfaf1c08a8ec3f18f2
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-27 14:25:19 +0330

    refactored...

--- src/components/aboutMe/AboutMeOtherSkills.tsx
+++ src/components/aboutMe/AboutMeOtherSkills.tsx
@@ -1,95 +1,10 @@
 "use client";
 import { SelfImprovementRounded } from "@mui/icons-material";
-import { Box, Chip, Theme } from "@mui/material";
+import { Box } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
+import otherSkillsData from "../data/otherSkillsData";
 import HeaderDivider from "../ui/HeaderDivider";
-import * as skillsIcons from "../../assets/icons/index";
-import Image from "next/image";
-import { SystemStyleObject } from "@mui/system";
-import { ResponsiveStyleValue } from "@mui/system/styleFunctionSx";
-
-const otherSkills = [
-  {
-    name: "redux",
-    icon: skillsIcons.ReduxIcon,
-    color: "#9900ff",
-  },
-  {
-    name: "react native",
-    icon: skillsIcons.reactIcon,
-    color: "#0059ff",
-  },
-  {
-    name: "materialUi",
-    icon: skillsIcons.MaterialUiIcon,
-    color: "#0087ff",
-  },
-  {
-    name: "tailwindCss",
-    icon: skillsIcons.TailwindCssIcon,
-    color: "#00fbff",
-  },
-  {
-    name: "vite",
-    icon: skillsIcons.ViteIcon,
-    color: "#9090ff",
-  },
-  {
-    name: "typescript",
-    icon: skillsIcons.TypescriptIcon,
-    color: "#aaaaaa",
-  },
-  {
-    name: "webpack",
-    icon: skillsIcons.WebpackIcon,
-    color: "#444466",
-  },
-  {
-    name: "socket.io",
-    icon: skillsIcons.SocketIoIcon,
-    color: "#aaaaaa",
-  },
-  {
-    name: "express",
-    icon: skillsIcons.ExpressJsIcon,
-    color: "#aaaaaa",
-  },
-  {
-    name: "jwt",
-    icon: skillsIcons.JwtIcon,
-    color: "#ffad00",
-  },
-  {
-    name: "TRPC",
-    icon: skillsIcons.TRPCIcon,
-    color: "#0055cc",
-  },
-  {
-    name: "prisma",
-    icon: skillsIcons.PrismaIcon,
-    color: "#53ff00",
-  },
-  {
-    name: "mongodb",
-    icon: skillsIcons.MongodbIcon,
-    color: "#00cf55",
-  },
-  {
-    name: "mySQL",
-    icon: skillsIcons.MySQL,
-    color: "#0088ff",
-  },
-  {
-    name: "postgresql",
-    icon: skillsIcons.PostgresqlIcon,
-    color: "#0099ff",
-  },
-  {
-    name: "redis",
-    icon: skillsIcons.RedisIcon,
-    color: "#ff5130",
-  },
-];
+import AboutMeSkillItem from "./AboutMeSkillItem";
 
 const AboutMeOtherSkills = () => {
   return (
@@ -104,26 +19,8 @@ const AboutMeOtherSkills = () => {
           سایر مهارت ها
         </HeaderDivider>
         <Box display="flex" flexWrap="wrap" justifyContent="center">
-          {otherSkills.map((skill) => (
-            <Chip
-              label={skill.name}
-              sx={{
-                bgcolor: skill.color,
-                color: "text.primary",
-                mt: "30px",
-                mx: "10px",
-                fontSize: "18px",
-                py: 2,
-              }}
-              icon={
-                <Image
-                  src={skill.icon.src}
-                  alt={skill.name}
-                  width={60}
-                  height={60}
-                />
-              }
-            />
+          {otherSkillsData.map((skill) => (
+            <AboutMeSkillItem key={skill.name} skill={skill} />
           ))}
         </Box>
       </Grid>
---
+++ src/components/aboutMe/AboutMeSkillItem.tsx
@@ -0,0 +1,30 @@
+import { Chip } from "@mui/material";
+import Image from "next/image";
+
+const AboutMeSkillItem = (props: {
+  skill: { name: string; color: string; icon: any };
+}) => {
+  return (
+    <Chip
+      label={props.skill.name}
+      sx={{
+        bgcolor: props.skill.color,
+        color: "text.primary",
+        mt: "30px",
+        mx: "10px",
+        fontSize: "18px",
+        py: 2,
+      }}
+      icon={
+        <Image
+          src={props.skill.icon.src}
+          alt={props.skill.name}
+          width={60}
+          height={60}
+        />
+      }
+    />
+  );
+};
+
+export default AboutMeSkillItem;
