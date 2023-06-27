f56bb7433fd1ffcb26caa0019912599537d2fd0f
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-27 11:42:52 +0330

    icon added to each chip

--- src/components/aboutMe/AboutMeOtherSkills.tsx
+++ src/components/aboutMe/AboutMeOtherSkills.tsx
@@ -3,11 +3,12 @@ import { SelfImprovementRounded } from "@mui/icons-material";
 import { Chip } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import HeaderDivider from "../ui/HeaderDivider";
+import { reactIcon as ReactNativeIcon } from "../../assets/icons/index";
 
 const otherSkills = [
   {
     name: "react native",
-    icon: "",
+    icon: <ReactNativeIcon />,
     color: "#0059ff",
   },
 ];
@@ -32,6 +33,7 @@ const AboutMeOtherSkills = () => {
                 sx={{
                   bgcolor: skill.color,
                 }}
+                icon={skill.icon}
               />
             </Grid>
           ))}
