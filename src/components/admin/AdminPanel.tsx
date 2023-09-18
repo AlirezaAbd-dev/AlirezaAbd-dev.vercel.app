69ae48b45ff958a0ef3702b0054d0aa00bf8073d
0AliReza0 <alireza.abedi9310@gmail.com>
2023-09-18 19:32:59 +0330

    MODIFIED: dbConnect added...

--- src/components/admin/AdminPanel.tsx
+++ src/components/admin/AdminPanel.tsx
@@ -1,12 +1,22 @@
 'use client';
+
+import { useContext, useEffect } from 'react';
 import { Box, Card, Typography } from '@mui/material';
+
 import Informations from './forms/Informations';
 import IntroductionForm from './forms/IntroductionForm';
 import SkillForm from './forms/SkillForm';
 import EducationForm from './forms/EducationForm';
 import ProjectForm from './forms/ProjectForm';
+import MainContext from '@/context/index';
 
 const AdminPanel = () => {
+   const { handlePageNumber } = useContext(MainContext);
+
+   useEffect(() => {
+      handlePageNumber.call(null, undefined, 4);
+   }, []);
+
    return (
       <Card
          sx={{
