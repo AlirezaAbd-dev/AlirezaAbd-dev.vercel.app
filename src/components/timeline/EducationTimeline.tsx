71317d73ed81c638e9e9223c758427f76ce1b2a4
0AliReza0 <alireza.abedi9310@gmail.com>
2023-10-03 22:14:49 +0330

    MODIFIED: about me page is half dynamic now...

--- src/components/timeline/EducationTimeline.tsx
+++ src/components/timeline/EducationTimeline.tsx
@@ -1,33 +1,46 @@
-"use client";
-import { WorkOutlineRounded } from "@mui/icons-material";
-import { Timeline } from "@mui/lab";
-import { Box } from "@mui/material";
+'use client';
+import { WorkOutlineRounded } from '@mui/icons-material';
+import { Timeline } from '@mui/lab';
+import { Box } from '@mui/material';
 
-import education from "../../constants/education";
-import HeaderDivider from "../../components/ui/HeaderDivider";
-import EducationTimelineItem from "../../components/timeline/EducationTimelineItem";
+import education from '../../constants/education';
+import HeaderDivider from '../../components/ui/HeaderDivider';
+import EducationTimelineItem from '../../components/timeline/EducationTimelineItem';
+import { useStore } from '@/store/store';
 
 const EducationTimeline = () => {
-  return (
-    <Box sx={{ mt: 2 }}>
-      <HeaderDivider
-        color="redAccent.main"
-        animation={false}
-        // @ts-ignore
-        icon={<WorkOutlineRounded color="text.primary" />}
-      >
-        تحصیلات
-      </HeaderDivider>
+   const educations = useStore((state) => state.data?.educations);
 
-      <Box display="flex" justifyContent="center" width="100%">
-        <Timeline position="right" sx={{ direction: "ltr" }}>
-          {education.map((item, index) => (
-            <EducationTimelineItem item={item} key={index} />
-          ))}
-        </Timeline>
+   return (
+      <Box sx={{ mt: 2 }}>
+         <HeaderDivider
+            color='redAccent.main'
+            animation={false}
+            // @ts-ignore
+            icon={<WorkOutlineRounded color='text.primary' />}
+         >
+            تحصیلات
+         </HeaderDivider>
+
+         <Box
+            display='flex'
+            justifyContent='center'
+            width='100%'
+         >
+            <Timeline
+               position='right'
+               sx={{ direction: 'ltr' }}
+            >
+               {educations?.map((item, index) => (
+                  <EducationTimelineItem
+                     item={item}
+                     key={item._id}
+                  />
+               ))}
+            </Timeline>
+         </Box>
       </Box>
-    </Box>
-  );
+   );
 };
 
 export default EducationTimeline;
