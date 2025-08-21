b911c41d18fb57eecb041b48bfd5911a379fd931
0AliReza0 <alireza.abedi9310@gmail.com>
2025-08-21 12:06:18 +0330

    profile page created

---
+++ src/app/(admin)/profile/page.tsx
@@ -0,0 +1,12 @@
+import { Card, Typography } from '@mui/material';
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <Card>
+      <Typography>Profile</Typography>
+    </Card>
+  );
+};
+
+export default Profile;
