1f70441600673600de4072021d58fb4801c350b8
alirezaabd-dev <alireza.abedi9310@gmail.com>
2026-08-16 22:39:55 +0330

    build error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,7 +74,13 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: '/favicon.ico',
+    icon: [
+      { url: '/favicon.ico', sizes: '32x32' },
+      { url: '/icon.png', sizes: '192x192', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
   },
 };
 
